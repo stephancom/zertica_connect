746ce136dafaac10e80c3875048bc8ef9c60c5a3
stephan.com <stephan@stephan.com>
2013-09-13 02:21:33 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZerticaConnect::Application.config.secret_key_base = 'fc0f73442d62e6e707bbe5c57837284f4013ac25f611496d97423fd3f964e83e45ce8e6364cf11747f78992c03cf56182fdecffbf8c3cd98c34002967642bebd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZerticaConnect::Application.config.session_store :cookie_store, key: '_zertica_connect_session'
