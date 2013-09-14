7c26879af830195466dcf0fa6f50610a087ed968
stephan.com <stephan@stephan.com>
2013-09-14 05:34:36 -0700

    unique user email
    yeah, I should do a migration, but we're so early on, I'm just going to blow the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+	validates :email, uniqueness: true
+
 	has_many :projects
 end
