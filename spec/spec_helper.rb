a6a4cfdd7d3366cf31c7a196b7094003fcd0d3de
stephan.com <stephan@stephan.com>
2013-09-14 05:34:52 -0700

    turn off turnip for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'turnip/capybara'
+# require 'turnip/capybara'
 require 'email_spec'
 require 'rspec/autorun'
 
