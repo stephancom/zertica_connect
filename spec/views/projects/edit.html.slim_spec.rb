0562ce710f904cefc37c3ab2bef599260768c0b0
stephan.com <stephan@stephan.com>
2013-09-14 23:35:36 -0700

    don't bother checking for lack of fields

--- spec/views/projects/edit.html.slim_spec.rb
+++ spec/views/projects/edit.html.slim_spec.rb
@@ -11,8 +11,6 @@ describe "projects/edit" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", project_path(@project), "post" do
       assert_select "input#project_title[name=?]", "project[title]"
-      # assert_select "input#project_state[name=?]", false
-      assert_select "select#project_user[name=?]", "project[user]"
       assert_select "textarea#project_spec[name=?]", "project[spec]"
     end
   end
--- spec/views/projects/new.html.slim_spec.rb
+++ spec/views/projects/new.html.slim_spec.rb
@@ -12,8 +12,6 @@ describe "projects/new" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", projects_path, "post" do
       assert_select "input#project_title[name=?]", "project[title]"
-      # assert_select "input#project_state[name=?]", false
-      assert_select "select#project_user[name=?]", "project[user]"
       assert_select "textarea#project_spec[name=?]", "project[spec]"
     end
   end
