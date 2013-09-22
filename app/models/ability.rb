180c0258fadeec4e6ee210e136397eb9ba9afe61
stephan.com <stephan@stephan.com>
2013-09-22 01:29:54 -0700

    Revert "dynamic shortcuts for roles"
    because it doesn't work on heroku :(
    
    This reverts commit edb2640e306fef41db255e433e0941755cf1ae1e.
    
    Conflicts:
    	app/views/projects/index.html.slim

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
     def initialize(user)
         user ||= User.new # guest user (not logged in)
 
-        if user.is_admin?
+        if user.has_role? :admin
             can :manage, :all
         end
 
