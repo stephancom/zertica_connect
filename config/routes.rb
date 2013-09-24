c9af277629bf60a94aa6c69e1a5d74872d517449
stephan.com <stephan@stephan.com>
2013-09-24 00:40:49 -0700

    admin registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ZerticaConnect::Application.routes.draw do	
-	devise_for :admins
+	devise_for :admins, :controllers => {:registrations => "admin_registrations"}
 	devise_for :users, :controllers => {:registrations => "registrations"}
 
 	authenticated :admin do
