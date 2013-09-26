976b88a8941180bd46543c30ba78411119c7d319
stephan.com <stephan@stephan.com>
2013-09-26 02:02:37 -0700

    eager load user and admin with active chat

--- app/models/active_chat.rb
+++ app/models/active_chat.rb
@@ -5,6 +5,8 @@ class ActiveChat < ActiveRecord::Base
   validates :user_id, uniqueness: {scope: :admin_id}, presence: true
   validates :admin_id, uniqueness: {scope: :user_id}, presence: true
 
+  default_scope includes(:user, :admin)
+
   delegate :name, to: :user
   delegate :name, to: :admin, prefix: true
 end
