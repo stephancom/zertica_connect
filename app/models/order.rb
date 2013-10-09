20522133d14f61f05f968684ff877a5f21a5b4a5
stephan.com <stephan@stephan.com>
2013-10-09 01:21:39 -0700

    add shippable files, required when shipping a cad order

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,15 +16,21 @@ class Order < ActiveRecord::Base
 	belongs_to :project
 	has_one :user, through: :project
 
+	# TODO
+	# assure project_files belong to project
 	has_and_belongs_to_many :project_files
+
+	has_and_belongs_to_many :shippable_files, class_name: 'ProjectFile', join_table: 'orders_shippable_files'
 
 	validates :title, presence: true
 	validates :order_type, presence: true, inclusion: { in: ORDER_TYPES }
 	validates :project, presence: true
 	validates :price, numericality: { greater_than: 0 }, allow_nil: true
-	# TODO
-	# validate project_files belong to project
 	validates :project_files, presence: true
+
+	# TODO
+	# validate payment confirmation correct if paid by user?
+	# validate tracking number correct format for carrier if print order?
 
 	delegate :title, to: :project, prefix: true
 	delegate :name, to: :user, prefix: true
@@ -136,9 +142,14 @@ class Order < ActiveRecord::Base
 		!confirmation.blank?
 	end
 
+	# if this is a cad order, make sure shippable files have been attached
+	# for a print order, make sure the tracking number is not blank
 	def shippable?
-		!tracking_number.blank?
-		# TODO if cad order, check for shippable files attached
+		if cad_order?
+			shippable_files.any?
+		else
+			!tracking_number.blank?
+		end
 	end
 end
 
