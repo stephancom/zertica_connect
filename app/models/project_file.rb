fa09cbb83cb8a77bc9a4e3d135e4821a9e1fdc1d
stephan.com <stephan@stephan.com>
2013-10-10 02:29:26 -0700

    add shipped_orders reverse habtl association to ProjectFile

--- app/models/project_file.rb
+++ app/models/project_file.rb
@@ -6,6 +6,7 @@ class ProjectFile < ActiveRecord::Base
   delegate :title, to: :project, prefix: true
 
   has_and_belongs_to_many :orders
+  has_and_belongs_to_many :shipped_orders, class_name: 'Order', join_table: 'orders_shippable_files'
 
   alias_attribute :name, :filename
 end
