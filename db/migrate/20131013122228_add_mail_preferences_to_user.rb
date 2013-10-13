ef27b3e160ad1a3bebe594e1147a264906045043
stephan.com <stephan@stephan.com>
2013-10-13 05:26:45 -0700

    mail preferences

---
+++ db/migrate/20131013122228_add_mail_preferences_to_user.rb
@@ -0,0 +1,9 @@
+class AddMailPreferencesToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :email_messages, :boolean, null: false, default: false
+    add_column :users, :email_estimate, :boolean, null: false, default: true
+    add_column :users, :email_production, :boolean, null: false, default: true
+    add_column :users, :email_complete, :boolean, null: false, default: true
+    add_column :users, :email_shipped, :boolean, null: false, default: true
+  end
+end
